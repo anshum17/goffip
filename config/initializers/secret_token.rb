fa476dd879ead44ab423fbd2b59e5b1e5391d6ab
anshrox17 <anshumagrawaliitr@gmail.com>
2015-05-29 15:41:23 +0530

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goffip::Application.config.secret_token = 'b1454157a591de523870ff0140aa225e8bd0ca3a134347c97be13abb68d32294387df4e81306104d31926971757df5515588702982b10d509c0140d4a9fb4833'
