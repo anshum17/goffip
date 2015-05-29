5c985567e0251483796f7cae9f065a565c88fd52
anshrox17 <anshumagrawaliitr@gmail.com>
2015-05-30 01:24:09 +0530

    fixing migration

--- db/migrate/20150529191522_changedefaultvaluepost.rb
+++ db/migrate/20150529191522_changedefaultvaluepost.rb
@@ -1,9 +1,9 @@
 class Changedefaultvaluepost < ActiveRecord::Migration
   def up
-    change_column :posts, :like, :integer, :default => []
-    change_column :comments, :like, :integer, :default => []
-    change_column :posts, :dislike, :integer, :default => []
-    change_column :comments, :dislike, :integer, :default => []
+    change_column :posts, :like, :array, :default => []
+    change_column :comments, :like, :array, :default => []
+    change_column :posts, :dislike, :array, :default => []
+    change_column :comments, :dislike, :array, :default => []
   end
 
   def down
