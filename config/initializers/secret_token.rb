0ff4c8026b8029233fa400caeb798022ea64d3d4
Akshay <akshay.bairagi25@gmail.com>
2015-05-29 17:14:10 +0530

    added user, post and comment models

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goffip::Application.config.secret_token = '388c8ddf81f074d1ae20736d20e25c5d1744cec0998ac538042d464f849a337fb45e6f8f0e6a307eeb387ccf0a953f5380c13ea05c011f369bd08349896daa00'
